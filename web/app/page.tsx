b8eb8f728d0b6f7f60e2e79704751fbace6c1060
25LP7910 25LP7910 <25LP7910@admins-iMac-3.local>
2026-03-10 17:42:08 +0800

    apollo client setup

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,30 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
 
-export default function Home() {
-  return <div>hello </div>;
+function App() {
+  useEffect(() => {
+    fetch("https://countries.trevorblades.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            countries {
+             
+              name
+              emoji
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return <div>Check console</div>;
 }
+
+export default App;
