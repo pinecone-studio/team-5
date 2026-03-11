d1eb3f73474ca1d348377526d34d4b0085ff9cf6
temuun-collab <buddowchinjawtemuunzaya@gmail.com>
2026-03-11 18:59:26 +0800

    fix to last change (#53)

--- service/drizzle.config.ts
+++ service/drizzle.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-	schema: "./src/db/schema.ts",
-	out: "./drizzle/migrations",
-	dialect: "sqlite",
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: "14a670312375f3f15fe2f605edb6b164",
-		databaseId: "14b46022-5b5d-4082-8432-73ab54dd7b7c",
-		token: "W4GMQ3OcI08pbwpBWCBhmhbhNHWosuQ5A29RVPZD"
-	}
+	schema: './src/db/schemas/*',
+	out: './drizzle/migrations',
+	dialect: 'sqlite',
 });
-
