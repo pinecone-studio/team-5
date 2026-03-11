69d95ebb839e9fb1e57f3218b8529d3b38ce8ab7
25LP9715 25LP9715 <25LP9715@admins-iMac.local>
2026-03-11 12:02:32 +0800

    jest

--- service/jest.config.js
+++ service/jest.config.js
@@ -1,11 +1,26 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.ts$": ["ts-jest", {
-      tsconfig: "test/tsconfig.json"
-    }],
-  },
+	testEnvironment: 'node',
+
+	transform: {
+		'^.+\\.ts$': [
+			'ts-jest',
+			{
+				tsconfig: 'test/tsconfig.json',
+			},
+		],
+	},
+
+	collectCoverage: true,
+
+	coverageThreshold: {
+		global: {
+			lines: 100,
+			statements: 100,
+			functions: 100,
+			branches: 100,
+		},
+	},
 };
