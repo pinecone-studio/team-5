a66577a8b916cc2c2881a3e29ab04cb11f4ab0b4
25LP9715 25LP9715 <25LP9715@admins-iMac.local>
2026-03-11 12:33:24 +0800

    jest test ignore files

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -5,11 +5,21 @@ const tsJestPreset = createDefaultPreset();
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/"],
   transform: {
     ...tsJestPreset.transform,
   },
   testMatch: ["**/*.test.ts"],
   collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/app/",
+    "<rootDir>/components/",
+    "<rootDir>/worker/",
+    "<rootDir>/lib/apollo-client.ts",
+    "<rootDir>/lib/queries.ts",
+    "<rootDir>/lib/utils.ts",
+  ],
 
   coverageThreshold: {
     global: {
