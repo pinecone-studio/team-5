87e42d844f5fb446c09ac2c7134307a2ceef3538
25LP9715 25LP9715 <25LP9715@admins-iMac.local>
2026-03-12 13:45:59 +0800

    fix: remove merge conflict markers

--- service/src/graphql/schemas/index.ts
+++ service/src/graphql/schemas/index.ts
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
 import { employeeTypeDefs } from "./employee.schema";
 import { benefitTypeDefs } from "./benefit.schema";
 import { contractTypeDefs } from "./contract.schema";
 import { eligibilityRuleTypeDefs } from "./eligibility_rule.schema";
 import { benefitEligibilityTypeDefs } from "./benefit_eligibility.schema";
 import { benefitRequestTypeDefs } from "./benefit_request.schema";
-=======
-import { employeeTypeDefs } from './employee.schema';
-import { benefitTypeDefs } from './benefit.schema';
-import { contractTypeDefs } from './contract.schema';
-import { eligibilityRuleTypeDefs } from './eligibility_rule.schema';
-import { benefitEligibilityTypeDefs } from './benefit_eligibility.schema';
-import { benefitRequestTypeDefs } from './benefit_request.schema';
->>>>>>> Stashed changes
 
 export const typeDefs = [
 	`
