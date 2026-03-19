b3da54a9b83075fafc682c4035bc6ded19201cd4
25LP9715 25LP9715 <25LP9715@admins-iMac.local>
2026-03-19 11:00:17 +0800

    add browser ison

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,9 +1,18 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ClerkAppProvider } from "@/components/apollo-provider";
 import { cn } from "@/lib/utils";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [{ url: "/favicon.png", type: "image/png", sizes: "53x57" }],
+    shortcut: [{ url: "/favicon.png", type: "image/png" }],
+    apple: [{ url: "/favicon.png", type: "image/png" }],
+  },
+};
 
 /**
  * Root layout.
